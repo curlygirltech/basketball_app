842d6ff5606d36f19d7c2091c3a7e4256613fa14
Jovonne Cameron <70806828+curlygirltech@users.noreply.github.com>
2023-08-23 12:55:49 -0400

    navbar

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 // Landing Home page
 import { Fragment } from "react";
 import Link from "next/link";
+import Stats from "./stats/index";
+import Home from "@/components/home-page/Home";
+import Nav from "@/components/home-page/Nav";
+
 
 const HomePage = () => {
   return (
     <Fragment>
+      <Nav /> 
       <h1>BASKETBALL STATISTICS HOME PAGE</h1>
       <ul>
         <li>
--- pages/stats/index.js
+++ pages/stats/index.js
@@ -1,14 +1,16 @@
 import Scraper from "../../utils/scraper";
 
+
 export default function Stats(props) {
   const playersAndSalaries = props.loadedData;
   return <div>Stats</div>;
 }
 
+
 export async function getServerSideProps() {
   let scraper = new Scraper();
   let data = await scraper.getStats();
-
+  
   if (!data) {
     return { notFound: true };
   }
